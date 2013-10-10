4f9a9e040f3eb12d733457388a6c8388f5d963d6
Aurbo99 <aurbo99@gmail.com>
2013-10-09 21:52:16 -0300

    Main Form movable and sticky, multi monitor support

--- Application/VixenApplication/VixenApplication.cs
+++ Application/VixenApplication/VixenApplication.cs
@@ -12,6 +12,7 @@ using Vixen.Services;
 using Vixen.Sys;
 using NLog;
 using Common.Controls;
+using VixenApplication.Properties;
 
 namespace VixenApplication
 {
@@ -28,6 +29,8 @@ namespace VixenApplication
 
 		private VixenApplicationData _applicationData;
 
+		bool windowInitialized;
+
 		public VixenApplication()
 		{
 			string[] args = Environment.GetCommandLineArgs();
@@ -42,6 +45,31 @@ namespace VixenApplication
 
 			stopping = false;
 			InitializeComponent();
+			this.WindowState = FormWindowState.Normal;
+			this.StartPosition = FormStartPosition.WindowsDefaultBounds;
+
+			// check if the saved bounds are nonzero and visible on any screen
+			if (Settings.Default.WindowPosition != Rectangle.Empty && IsVisibleOnAnyScreen(Settings.Default.WindowPosition))
+			{
+				// first set the bounds
+				this.StartPosition = FormStartPosition.Manual;
+				this.DesktopBounds = Settings.Default.WindowPosition;
+
+				// afterwards set the window state to the saved value (which could be Maximized)
+				this.WindowState = Settings.Default.WindowState;
+			}
+			else
+			{
+				// this resets the upper left corner of the window to windows standards
+				this.StartPosition = FormStartPosition.WindowsDefaultLocation;
+
+				// we can still apply the saved size
+				if (Settings.Default.WindowPosition != Rectangle.Empty)
+				{
+					this.Size = Settings.Default.WindowPosition.Size;
+				}
+			}
+			windowInitialized = true;
 			PopulateVersionStrings();
 			AppCommands = new AppCommand(this);
 			Execution.ExecutionStateChanged += executionStateChangedHandler;
@@ -49,6 +77,77 @@ namespace VixenApplication
 
 			InitStats();
 		}
+
+		private bool IsVisibleOnAnyScreen(Rectangle rect)
+		{
+			foreach (Screen screen in Screen.AllScreens)
+			{
+				if (screen.WorkingArea.IntersectsWith(rect))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		protected override void OnClosed(EventArgs e)
+		{
+			base.OnClosed(e);
+
+			// only save the WindowState if Normal or Maximized
+			switch (this.WindowState)
+			{
+				case FormWindowState.Normal:
+				case FormWindowState.Maximized:
+					Settings.Default.WindowState = this.WindowState;
+					break;
+
+				default:
+					Settings.Default.WindowState = FormWindowState.Normal;
+					break;
+			}
+
+			# region this code does *not* handle minimized/maximized window.
+
+			// reset window state to normal to get the correct bounds
+			// also make the form invisible to prevent distracting the user
+			//this.Visible = false;
+			//this.WindowState = FormWindowState.Normal;
+			//Settings.Default.WindowPosition = this.DesktopBounds;
+
+			# endregion
+
+			Settings.Default.Save();
+		}
+
+		# region window size/position
+		// Added region to handle closing when window is minimized or maximized.
+
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+			TrackWindowState();
+		}
+
+		protected override void OnMove(EventArgs e)
+		{
+			base.OnMove(e);
+			TrackWindowState();
+		}
+
+		// On a move or resize in Normal state, record the new values as they occur.
+		// This solves the problem of closing the app when minimized or maximized.
+		private void TrackWindowState()
+		{
+			// Don't record the window setup, otherwise we lose the persistent values!
+			if (!windowInitialized) { return; }
+			if (WindowState == FormWindowState.Normal)
+			{
+				Settings.Default.WindowPosition = this.DesktopBounds;
+			}
+		}
+
+		# endregion window size/position
 
 
 		private void VixenApp_FormClosing(object sender, FormClosingEventArgs e)
