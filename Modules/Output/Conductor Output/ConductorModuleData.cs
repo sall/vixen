81ff1d314a9a0353b6748180124b4e73b86176e8
Egenoup <cstrang@funfor.us>
2013-11-09 10:18:47 -0600

    Adding Conductor Output module to Egenoup's Master

---
+++ Modules/Output/Conductor Output/ConductorModuleData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vixen;
+using Vixen.Module;
+using System.Runtime.Serialization;
+
+
+namespace VixenModules.Output.ConductorOutput
+{
+    [DataContract]
+    public class ConductorModuleData : ModuleDataModelBase
+    {
+        [DataMember]
+        public Boolean savedata;
+
+        public override IModuleDataModel Clone()
+        {
+            return MemberwiseClone() as IModuleDataModel;
+        }
+    }
+}
