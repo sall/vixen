2565f260b9645e1987f19fb510c027db5b926385
Darren McDaniel <gizmohd@gmail.com>
2015-01-11 14:57:12 -0500

    Merge changes from sall/vixen

---
+++ Common/Controls/IPTextBox.cs
@@ -0,0 +1,19 @@
+﻿namespace Common.Controls
+{
+	using System.Windows.Forms;
+
+	public class IpTextBox : TextBox
+	{
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				var createParams = base.CreateParams;
+				createParams.ClassName = "SysIPAddress32";
+				createParams.Height = 23;
+                Font = (System.Drawing.Font)Font.Clone();
+				return createParams;
+			}
+		}
+	}
+}
